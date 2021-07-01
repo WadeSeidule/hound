df3e2d9cb0df797679dbdb8f8647f888347527e1
wseidule <wseidule@hyperfine.io>
2021-07-01 16:36:54 -0400

    factor out ssh key path

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-docker build --build-arg SSH_KEY="$(cat ~/.ssh/id_ed25519)" -t hound . &&
+SSH_KEY_PATH=~/.ssh/id_ed25519
+docker build --build-arg SSH_KEY="$(cat $SSH_KEY_PATH)" -t hound . &&
 docker run -d -p 6080:6080 --name hound -v $(pwd):/data -t hound
